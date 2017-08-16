a2fdb7158570964caa3871abfb217d6dc00c9b5a
Irakli Tchitadze <bug@iraklis-mbp.home>
2017-08-15 21:41:29 -0400

    Initial Commit

--- PiOSDrone/ViewController.swift
+++ PiOSDrone/ViewController.swift
@@ -7,11 +7,27 @@
 //
 
 import UIKit
+import SocketIO
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider! {
+    	didSet{
+    		slider.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi / 2))
+    	}
+    }
+    
+    var globalThrottleValue: Int = 0;
+
+    @IBOutlet weak var throttle: UILabel!
+    
+    let socket = SocketIOClient(socketURL: URL(string: "http://192.168.1.2:3000")!, config: [.log(true), .compress]);
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        connect()
+        sendMessage(value: "Hello Socket.IO!!!")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +36,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func throttleChanged(_ sender: UISlider) {
+        let throttleVal = Int(self.slider.value)
+        if globalThrottleValue != throttleVal {
+            globalThrottleValue = throttleVal
+            throttle.text = String(throttleVal)
+            sendMessage(value: String(throttleVal))
+        }
+    }
+    
+    func connect() {
+        socket.on(clientEvent: .connect) {data, ack in
+            print("socket connected")
+        }
+        
+        socket.connect()
+    }
+    
+    func sendMessage(value: String) {
+        socket.emit("data", value)
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'PiOSDrone' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PiOSDrone
+  pod 'Socket.IO-Client-Swift', '~> 11.0.2' # Or latest version
+
+end
